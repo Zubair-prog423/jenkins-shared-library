6d97f6b63505a31767db7d9acb88e020c9a8a9c8
Zubair-prog423 <sadabkhan639580@gmail.com>
2024-10-23 16:55:18 +0530

    Update first_resource.groovy

--- vars/first_resource.groovy
+++ vars/first_resource.groovy
@@ -1,30 +1,6 @@
-def call(givenConfig = [:]) {
-  def defaultConfig = [
-    /**
-      * The Jenkins node, or label, that will be allocated for this build.
-      */
-    "jenkinsNode": "BUILD",
-    /**
-      * All config specific to NPM repo type.
-      */
-    "npm": [
-      /**
-        * Whether or not to run Cypress tests, if there are any.
-        */
-      "cypress": true
-    ],
-    "maven": [
-      /**
-        * Whether or not to run integration tests, if there are any.
-        */
-      "integTest": true
-    ]
-  ]
-  // https://e.printstacktrace.blog/how-to-merge-two-maps-in-groovy/
-  def effectiveConfig merge(defaultConfig, givenConfig)
-  println "Configuration is documented here: https://whereverYouHos/getConfig.groovy"
-  println "Default config: " + defaultConfig
-  println "Given config: " + givenConfig
-  println "Effective config: " + effectiveConfig
-  return effectiveConfig
+def call()
+{
+  sh "date"
+  sh "ifconfig enp0s3
 }
+  
